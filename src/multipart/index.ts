5c306efaa1857e534283c9782c49f64d5d196325
xnerhu <xnerhu@gmail.com>
2021-10-02 12:36:19 +0200

    fix: remove filter callback from exports

--- src/multipart/index.ts
+++ src/multipart/index.ts
@@ -1,7 +1,3 @@
 export * from "./options";
 export * from "./filter";
-export {
-  UploadFilterFile,
-  UploadFilterHandler,
-  UploadFilterHandlerCallback,
-} from "./filter";
+export { UploadFilterFile, UploadFilterHandler } from "./filter";
